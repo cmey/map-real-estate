f5c88d24204891034fe8123b8f4f3df9a72ba1b1
Christophe Meyer <cmeyer@butterflynetinc.com>
2017-09-17 19:29:49 -0400

    Add setup script to install deps

---
+++ setup.sh
@@ -0,0 +1,5 @@
+brew install geos
+pyenv install 3.6.1
+pyenv virtual 3.6.1 map-real-estate-3.6.1
+pip install geopy matplotlib pandas
+pip install https://github.com/matplotlib/basemap/archive/v1.1.0.tar.gz
