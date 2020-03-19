5771a4405e2895eae5fd2462deded76ffc6ae12b
Christophe Meyer <christophe.meyer.pro@gmail.com>
2020-03-19 10:47:02 +0100

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 brew install geos
 pyenv install 3.6.1
-pyenv virtual 3.6.1 map-real-estate-3.6.1
+pyenv virtualenv 3.6.1 map-real-estate-3.6.1
+pyenv local map-real-estate-3.6.1
 pip install geopy matplotlib pandas
 pip install https://github.com/matplotlib/basemap/archive/v1.1.0.tar.gz
